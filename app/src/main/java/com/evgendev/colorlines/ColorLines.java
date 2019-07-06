0749541e2c74e06f22c6f1eecad6fdff3ba01af2
Evgeny268 <evgepishcin@gmail.com>
2019-07-06 21:55:43 +0300

    realise constructors and generateNextColors

--- app/src/main/java/com/evgendev/colorlines/ColorLines.java
+++ app/src/main/java/com/evgendev/colorlines/ColorLines.java
@@ -1,4 +1,43 @@
 package com.evgendev.colorlines;
 
 public class ColorLines {
+
+    private int fieldSize; //Размер игрового поля
+    private int [][]field; //Игровое поле (0 - пусто, >0 - цвет шара)
+    private int []nextColors; //Цвет шаров, которые появятся при следующем ходе
+    private int score = 0; //Набранные очки
+    private int colorsCount = 7; //Кол-во всех цветов
+    private int nextBallsCount = 3; //Кол-во генерируемых шаров за раз
+    private int collapseCount = 5; //Кол-во шаров, которое нужно собрать в линию
+
+    public ColorLines(int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new int[fieldSize][fieldSize];
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                field[i][j] = 0;
+            }
+        }
+    }
+
+    public ColorLines(int fieldSize, int colorsCount, int nextBallsCount, int collapseCount) {
+        this(fieldSize);
+        this.colorsCount = colorsCount;
+        this.nextBallsCount = nextBallsCount;
+        this.collapseCount = collapseCount;
+    }
+
+    private int []generateNextColors(int balls, int colors){
+        int []arr = new int[balls];
+        for (int i = 0; i < balls; i++) {
+            arr[i] = rnd(1,colors);
+        }
+        return arr;
+    }
+
+    public static int rnd(int min, int max)
+    {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
 }
