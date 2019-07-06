0ba33665d20eea815155dc49c17efa7183cf5abc
Evgeny268 <evgepishcin@gmail.com>
2019-07-06 22:40:29 +0300

    realise method addBalls

--- app/src/main/java/com/evgendev/colorlines/ColorLines.java
+++ app/src/main/java/com/evgendev/colorlines/ColorLines.java
@@ -1,10 +1,13 @@
 package com.evgendev.colorlines;
 
+import java.util.ArrayList;
+
 public class ColorLines {
 
     private int fieldSize; //Размер игрового поля
     private int [][]field; //Игровое поле (0 - пусто, >0 - цвет шара)
     private int []nextColors; //Цвет шаров, которые появятся при следующем ходе
+    private int []selectedBall = {-1,-1}; //Выбранные шар
     private int score = 0; //Набранные очки
     private int colorsCount = 7; //Кол-во всех цветов
     private int nextBallsCount = 3; //Кол-во генерируемых шаров за раз
@@ -33,6 +36,24 @@ public class ColorLines {
             arr[i] = rnd(1,colors);
         }
         return arr;
+    }
+
+    private int [][]addBalls(int [][]oldField, int []newBalls){
+        ArrayList<Integer> freeIndex = new ArrayList<>();
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                if (oldField[i][j]==0){
+                    freeIndex.add(fieldSize*i+j);
+                }
+            }
+        }
+        for (int i = 0; i < newBalls.length; i++) {
+            if (freeIndex.size()<=0) break;
+            int rndIndex = rnd(0,freeIndex.size()-1);
+            oldField[freeIndex.get(rndIndex)/fieldSize][freeIndex.get(rndIndex)-((freeIndex.get(rndIndex)/fieldSize)*fieldSize)] = newBalls[i];
+            freeIndex.remove(rndIndex);
+        }
+        return oldField;
     }
 
     public static int rnd(int min, int max)
