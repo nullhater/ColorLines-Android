0d90a6fd84a5b25f4c09f10ef979462c82a2622a
Evgeny268 <evgepishcin@gmail.com>
2019-07-14 12:34:01 +0300

    realize app utils

---
+++ app/src/main/java/com/evgendev/colorlines/AppUtils.java
@@ -0,0 +1,53 @@
+package com.evgendev.colorlines;
+
+
+
+import android.content.Context;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class AppUtils {
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final String APP_PREFERENCES_SAVEGAME = "savegame";
+    public static final String FILESAVE = "fsave";
+
+
+    public static void objToFile(String filepath, Object object, Context context){
+        try {
+            FileOutputStream fos = context.openFileOutput(filepath, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(object);
+            os.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object FileToObj(String filepath, Context context){
+        try {
+            FileInputStream fis = context.openFileInput(filepath);
+            ObjectInputStream is = new ObjectInputStream(fis);
+            Object simpleClass = is.readObject();
+            is.close();
+            fis.close();
+            return simpleClass;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
