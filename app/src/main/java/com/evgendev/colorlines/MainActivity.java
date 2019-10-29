fb55225a270cafdf85e709aa6bc645371257f8ff
Evgeny268 <evgepishcin@gmail.com>
2019-10-30 02:17:37 +0300

    minimize the application when you press the back button

--- app/src/main/java/com/evgendev/colorlines/MainActivity.java
+++ app/src/main/java/com/evgendev/colorlines/MainActivity.java
@@ -63,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 }
