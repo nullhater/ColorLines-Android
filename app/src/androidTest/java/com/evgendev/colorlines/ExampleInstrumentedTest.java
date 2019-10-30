644bb3383868a1535eda3909ce6c4a6392005fc3
Evgeny268 <evgepishcin@gmail.com>
2019-10-30 15:19:19 +0300

    add some test

--- app/src/androidTest/java/com/evgendev/colorlines/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/evgendev/colorlines/ExampleInstrumentedTest.java
@@ -32,4 +32,471 @@ public class ExampleInstrumentedTest {
         Integer b = (Integer) AppUtils.FileToObj("test",appContext);
         assertEquals(new Integer(4),b);
     }
+
+        @Test
+    public void delete_balls_1(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][0] = 1;
+        colorLines.getField()[0][1] = 1;
+        colorLines.getField()[0][2] = 1;
+        colorLines.getField()[0][3] = 1;
+        colorLines.getField()[0][4] = 1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_2(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][4] = 6;
+        colorLines.getField()[0][5] = 6;
+        colorLines.getField()[0][6] = 6;
+        colorLines.getField()[0][7] = 6;
+        colorLines.getField()[0][8] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_3(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[2][4] = 6;
+        colorLines.getField()[2][5] = 6;
+        colorLines.getField()[2][6] = 6;
+        colorLines.getField()[2][7] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_4(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[2][4] = 6;
+        colorLines.getField()[2][5] = 6;
+        colorLines.getField()[2][6] = 6;
+        colorLines.getField()[2][7] = 6;
+        colorLines.getField()[3][3] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_5(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[2][0] = 6;
+        colorLines.getField()[2][1] = 6;
+        colorLines.getField()[2][2] = 6;
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[2][4] = 6;
+        colorLines.getField()[2][5] = 6;
+        colorLines.getField()[2][6] = 6;
+        colorLines.getField()[2][7] = 6;
+        colorLines.getField()[2][8] = 6;
+        assertEquals(18,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_6(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][0] = 1;
+        colorLines.getField()[1][0] = 1;
+        colorLines.getField()[2][0] = 1;
+        colorLines.getField()[3][0] = 1;
+        colorLines.getField()[4][0] = 1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_7(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[3][2] = 6;
+        colorLines.getField()[4][2] = 6;
+        colorLines.getField()[5][2] = 6;
+        colorLines.getField()[6][2] = 6;
+        colorLines.getField()[7][2] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_8(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[4][2] = 6;
+        colorLines.getField()[5][2] = 6;
+        colorLines.getField()[6][2] = 6;
+        colorLines.getField()[7][2] = 6;
+        colorLines.getField()[8][2] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_9(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][3] = 6;
+        colorLines.getField()[1][3] = 6;
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[3][3] = 6;
+        colorLines.getField()[4][3] = 6;
+        colorLines.getField()[5][3] = 6;
+        colorLines.getField()[6][3] = 6;
+        colorLines.getField()[7][3] = 6;
+        colorLines.getField()[8][3] = 6;
+        assertEquals(18,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_10(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[1][0] = 0;
+        colorLines.getField()[1][1] = 1;
+        colorLines.getField()[1][2] = 1;
+        colorLines.getField()[1][3] = 6;
+        colorLines.getField()[1][4] = 6;
+        colorLines.getField()[1][5] = 6;
+        colorLines.getField()[1][6] = 6;
+        colorLines.getField()[1][7] = 6;
+        colorLines.getField()[1][8] = 2;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_1(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][0] = 6;
+        colorLines.getField()[1][1] = 6;
+        colorLines.getField()[2][2] = 6;
+        colorLines.getField()[3][3] = 6;
+        colorLines.getField()[4][4] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_2(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[4][4] = 6;
+        colorLines.getField()[5][5] = 6;
+        colorLines.getField()[6][6] = 6;
+        colorLines.getField()[7][7] = 6;
+        colorLines.getField()[8][8] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_3(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][4] = 6;
+        colorLines.getField()[1][5] = 6;
+        colorLines.getField()[2][6] = 6;
+        colorLines.getField()[3][7] = 6;
+        colorLines.getField()[4][8] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_4(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[4][0] = 6;
+        colorLines.getField()[5][1] = 6;
+        colorLines.getField()[6][2] = 6;
+        colorLines.getField()[7][3] = 6;
+        colorLines.getField()[8][4] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_5(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][8] = 6;
+        colorLines.getField()[1][7] = 6;
+        colorLines.getField()[2][6] = 6;
+        colorLines.getField()[3][5] = 6;
+        colorLines.getField()[4][4] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_6(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[8][0] = 6;
+        colorLines.getField()[7][1] = 6;
+        colorLines.getField()[6][2] = 6;
+        colorLines.getField()[5][3] = 6;
+        colorLines.getField()[4][4] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_7(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[7][1] = 6;
+        colorLines.getField()[6][2] = 6;
+        colorLines.getField()[5][3] = 6;
+        colorLines.getField()[4][4] = 6;
+        colorLines.getField()[3][5] = 6;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_8(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][0] = 6;
+        colorLines.getField()[1][1] = 6;
+        colorLines.getField()[2][2] = 6;
+        colorLines.getField()[3][3] = 6;
+        colorLines.getField()[4][4] = 6;
+        colorLines.getField()[5][5] = 6;
+        colorLines.getField()[6][6] = 6;
+        colorLines.getField()[7][7] = 6;
+        colorLines.getField()[8][8] = 6;
+        assertEquals(18,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_9(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[1][0] = 6;
+        colorLines.getField()[2][1] = 6;
+        colorLines.getField()[3][2] = 6;
+        colorLines.getField()[4][3] = 6;
+        colorLines.getField()[5][4] = 6;
+        colorLines.getField()[6][5] = 6;
+        colorLines.getField()[7][6] = 6;
+        colorLines.getField()[8][7] = 6;
+        assertEquals(16,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_10(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][1] = 6;
+        colorLines.getField()[1][2] = 6;
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[3][4] = 6;
+        colorLines.getField()[4][5] = 6;
+        colorLines.getField()[5][6] = 6;
+        colorLines.getField()[6][7] = 6;
+        colorLines.getField()[7][8] = 6;
+        assertEquals(16,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_11(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][1] = 2;
+        colorLines.getField()[1][2] = 2;
+        colorLines.getField()[2][3] = 6;
+        colorLines.getField()[3][4] = 6;
+        colorLines.getField()[4][5] = 6;
+        colorLines.getField()[5][6] = 6;
+        colorLines.getField()[6][7] = 6;
+        colorLines.getField()[7][8] = 1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void delete_balls_diagonal_12(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][0] = 0;
+        colorLines.getField()[1][1] = 2;
+        colorLines.getField()[2][2] = 2;
+        colorLines.getField()[3][3] = 6;
+        colorLines.getField()[4][4] = 6;
+        colorLines.getField()[5][5] = 6;
+        colorLines.getField()[6][6] = 6;
+        colorLines.getField()[7][7] = 6;
+        colorLines.getField()[8][8] = 1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void leftUpDiagonal(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][4]=1;
+        colorLines.getField()[1][3]=1;
+        colorLines.getField()[2][2]=1;
+        colorLines.getField()[3][1]=1;
+        colorLines.getField()[4][0]=1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void leftUpDiagonal_2(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[1][4]=1;
+        colorLines.getField()[2][3]=1;
+        colorLines.getField()[3][2]=1;
+        colorLines.getField()[4][1]=1;
+        colorLines.getField()[5][0]=1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void leftUpDiagonal_3(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[0][5]=1;
+        colorLines.getField()[1][4]=1;
+        colorLines.getField()[2][3]=1;
+        colorLines.getField()[3][2]=1;
+        colorLines.getField()[4][1]=1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void rightUpDiagonal_1(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[3][8]=1;
+        colorLines.getField()[4][7]=1;
+        colorLines.getField()[5][6]=1;
+        colorLines.getField()[6][5]=1;
+        colorLines.getField()[7][4]=1;
+        colorLines.getField()[4][8]=2;
+        colorLines.getField()[8][4]=3;
+        colorLines.getField()[8][5]=3;
+        colorLines.getField()[7][5]=3;
+        assertEquals(10,colorLines.checkLines());
+    }
+
+    @Test
+    public void rightUpDiagonal_2(){
+        ColorLines colorLines = new ColorLines(9);
+        for (int i = 0; i < colorLines.getFieldSize(); i++) {
+            for (int j = 0; j < colorLines.getFieldSize(); j++) {
+                colorLines.getField()[i][j] = 0;
+            }
+        }
+        colorLines.getField()[8][3]=1;
+        colorLines.getField()[7][4]=1;
+        colorLines.getField()[6][5]=1;
+        colorLines.getField()[5][6]=1;
+        colorLines.getField()[4][7]=1;
+        assertEquals(10,colorLines.checkLines());
+    }
+
 }
